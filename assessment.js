ca6c6c730f81d7996001e7e6540037e7a7900737
Inari100 <145836086+Inari100@users.noreply.github.com>
2023-09-24 12:36:56 +0900

    修正

--- assessment.js
+++ assessment.js
@@ -11,17 +11,30 @@ assessmentButton.onclick = () => {
     return;
   }
 
-  // 診断結果表示エリアの作成
+   // 診断結果表示エリアの作成
   resultDivision.innerText = '';
-  const header = document.createElement('h3');
-  header.innerText = '診断結果';
-  resultDivision.appendChild(header);
+  
+// headerDivisionの作成
+const headerDivision = document.createElement('div');
+headerDivision.setAttribute('class','card-header text-bg-primary');
+headerDivision.innerText = '診断結果';
 
-  const paragraph = document.createElement('p');
-  const result = assessment(userName);
-  paragraph.innerText = result;
-  resultDivision.appendChild(paragraph);
+// bodyDivisionの作成
+const bodyDivision = document.createElement('div');
+bodyDivision.setAttribute('class','card-body');
 
+const paragraph = document.createElement('p');
+paragraph.setAttribute('class','card-text');
+const result = assessment(userName);
+paragraph.innerText = result;
+bodyDivision.appendChild(paragraph);
+
+// resultDivisionにBootstrapのスタイルを適用する
+resultDivision.setAttribute('class','card');
+
+//resultDivisionとbodyDIvisionをresultDivisionに差し込む
+resultDivision.appendChild(headerDivision);
+resultDivision.appendChild(bodyDivision);
   // ツイートエリアの作成
   tweetDivision.innerText = '';
   const anchor = document.createElement('a');
